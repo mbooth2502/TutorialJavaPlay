f56e7785ce66565221fd3f86427f8a86063af4e4
mbooth2502 <boothm@talktalk.net>
2025-05-15 14:24:04 +0100

    Used renderArgs to pass title/baseline, Updated main.css

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -4,10 +4,17 @@ import java.util.*;
  
 import play.*;
 import play.mvc.*;
+import play.Play;
  
 import models.*;
  
 public class Application extends Controller {
+
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
  
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
